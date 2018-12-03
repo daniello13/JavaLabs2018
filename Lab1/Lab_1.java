9dfb802cc2c9fc32b2a731da45239904231f0261
Daniel Artemiev <daniel.artemiev@outlook.com>
2018-12-03 07:11:19 +0300

    lab1_ready

---
+++ Lab1/Lab_1.java
@@ -0,0 +1,24 @@
+public class Lab_1
+{
+	/**
+	* @author Daniel Artemiev
+	* @ version 1.0.0
+	* @since 03.12.18
+	**/
+	
+    public static void main(String[] args) {
+		/**
+		* @param args Unused.
+		* @return Nothing.
+		**/
+		//hello world
+        System.out.println("Group: 545a");
+        System.out.println("Student: Artemiev Daniel Ivanovich");
+        System.out.println("Variant: 1");
+		System.out.println("_$$$$___$$$$$___$$$$$$");
+		System.out.println("$$__$$__$$__$$____$$");
+		System.out.println("$$$$$$__$$__$$____$$");
+		System.out.println("$$__$$__$$__$$____$$");
+		System.out.println("$$__$$__$$$$$___$$$$$$");
+    }
+}
